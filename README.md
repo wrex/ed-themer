9778a42660474806fb57d5454cf38d0c097055e1
Rex Walters <653208+wrex@users.noreply.github.com>
2023-02-09 12:12:00 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Elementary Dark Themer
+
+This is an auxiliary app for the [WaniKani Elementary
+Dark](https://github.com/Sepitus-exe/WKElementaryDark) project.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<h1>Elementary Dark Themer</h1>
+<div class="pickers">
+  <label for="surface-1">--USER-surface-1</label>
+  <input type="color" name="surface-1" id="surface-1">
+  <label for="surface-2">--USER-surface-2</label>
+  <input type="color" name="surface-1" id="surface-1">
+  <label for="surface-3">--USER-surface-3</label>
+  <input type="color" name="surface-1" id="surface-1">
+  <label for="surface-4">--USER-surface-4</label>
+  <input type="color" name="surface-1" id="surface-1">
+  <label for="inverted-surface">--USER-inverted-surface</label>
+  <input type="color" name="inverted-surface" id="inverted-surface">
+
+</div>
+
+
+<style>
+</style>
