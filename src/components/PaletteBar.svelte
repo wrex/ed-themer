f8e974b36d2329d6ae45f4314727353413f8bce4
Rex Walters <653208+wrex@users.noreply.github.com>
2023-02-12 15:20:51 -0800

    errant console.log

--- src/components/PaletteBar.svelte
+++ src/components/PaletteBar.svelte
@@ -1,12 +1,10 @@
 <script>
-  export let id = "foo";
-  
-  import {tint, shade} from "$lib/utils";
+	export let id = 'foo';
 
-  console.log(shade("#7e1111", 0.2))
+	import { tint, shade } from '$lib/utils';
 
-  let baseClr = "#888888";
-  $: customProps = `
+	let baseClr = '#888888';
+	$: customProps = `
   --baseClr: ${baseClr};
   --t13: ${tint(baseClr, 0.13)};
   --t26: ${tint(baseClr, 0.26)};
@@ -26,84 +24,83 @@
 </script>
 
 <div class="container" style={customProps}>
-  <div class="swatch s91"></div>
-  <div class="swatch s78"></div>
-  <div class="swatch s65"></div>
-  <div class="swatch s52"></div>
-  <div class="swatch s39"></div>
-  <div class="swatch s26"></div>
-  <div class="swatch s13"></div>
-  <input type="color" name={id} id={id} bind:value={baseClr}>
-  <div class="swatch t13"></div>
-  <div class="swatch t26"></div>
-  <div class="swatch t39"></div>
-  <div class="swatch t52"></div>
-  <div class="swatch t65"></div>
-  <div class="swatch t78"></div>
-  <div class="swatch t91"></div>
+	<div class="swatch s91" />
+	<div class="swatch s78" />
+	<div class="swatch s65" />
+	<div class="swatch s52" />
+	<div class="swatch s39" />
+	<div class="swatch s26" />
+	<div class="swatch s13" />
+	<input type="color" name={id} {id} bind:value={baseClr} />
+	<div class="swatch t13" />
+	<div class="swatch t26" />
+	<div class="swatch t39" />
+	<div class="swatch t52" />
+	<div class="swatch t65" />
+	<div class="swatch t78" />
+	<div class="swatch t91" />
 </div>
 
 <style>
-  .container {
-    padding: 0.25em 0;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .container > input {
-    width: 50vw;
-    margin: 0;
-    padding: 0.5em;
-  }
+	.container {
+		padding: 0.25em 0;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.container > input {
+		width: 50vw;
+		margin: 0;
+		padding: 0.5em;
+	}
 
-  .swatch {
-    border: 1px solid var(--baseClr);
-    height: 50px;
-    width: 100vw;
-  }
+	.swatch {
+		border: 1px solid var(--baseClr);
+		height: 50px;
+		width: 100vw;
+	}
 
-  .t13 {
-    background-color: var(--t13);
-  }
-  .t26 {
-    background-color: var(--t26);
-  }
-  .t39 {
-    background-color: var(--t39);
-  }
-  .t52 {
-    background-color: var(--t52);
-  }
-  .t65 {
-    background-color: var(--t65);
-  }
-  .t78 {
-    background-color: var(--t78);
-  }
-  .t91 {
-    background-color: var(--t91);
-  }
+	.t13 {
+		background-color: var(--t13);
+	}
+	.t26 {
+		background-color: var(--t26);
+	}
+	.t39 {
+		background-color: var(--t39);
+	}
+	.t52 {
+		background-color: var(--t52);
+	}
+	.t65 {
+		background-color: var(--t65);
+	}
+	.t78 {
+		background-color: var(--t78);
+	}
+	.t91 {
+		background-color: var(--t91);
+	}
 
-  .s13 {
-    background-color: var(--s13);
-  }
-  .s26 {
-    background-color: var(--s26);
-  }
-  .s39 {
-    background-color: var(--s39);
-  }
-  .s52 {
-    background-color: var(--s52);
-  }
-  .s65 {
-    background-color: var(--s65);
-  }
-  .s78 {
-    background-color: var(--s78);
-  }
-  .s91 {
-    background-color: var(--s91);
-  }
-
+	.s13 {
+		background-color: var(--s13);
+	}
+	.s26 {
+		background-color: var(--s26);
+	}
+	.s39 {
+		background-color: var(--s39);
+	}
+	.s52 {
+		background-color: var(--s52);
+	}
+	.s65 {
+		background-color: var(--s65);
+	}
+	.s78 {
+		background-color: var(--s78);
+	}
+	.s91 {
+		background-color: var(--s91);
+	}
 </style>
